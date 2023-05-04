45cc7117a5108d0e6b1076f9f6dccc3046c53d7e
KME Hasan <mugdha71@gmail.com>
2023-05-05 01:02:10 +0600

    headless

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from Browser import Browser
+from BrowserFirefox import Browser
 
 if __name__ == '__main__':
-    browser = Browser()
     with open('input.txt','r', encoding='utf-8') as f:
         urls = f.read().split('\n')
         for url in urls:
+            browser = Browser()
             if url == '':
                 continue
             browser.getAlldataFromUrl(url)
-        browser.quit()
+            browser.quit()
